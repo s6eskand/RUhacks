c14f6e040158d78fcc405250fe559745edb1b1fb
Christopher Santos <chrissantos221@gmail.com>
2020-05-16 17:54:34 -0400

    adding calendar - compiles but doesn't work

---
+++ frontend/src/Views/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import EventCalendar from './component/EventCalendar';
+
+function App(){
+    return (
+        <EventCalendar/>
+
+    )
+}
+
+export default App;
---
+++ frontend/src/Views/eventCalendar.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+export default class EventCalendar extends Component  {
+
+
+    render(){
+        return (
+            <FullCalendar />
+
+        )
+    }
+}
+
