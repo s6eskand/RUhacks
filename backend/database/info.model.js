bbd1583fc7be516e43d112ed7b96ebac73cb3c54
Sam <sam.eskandar00@hotmail.com>
2020-05-16 18:07:40 -0400

    update 'Info' collection

--- backend/database/info.model.js
+++ backend/database/info.model.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let Info = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type:  String,
+        required: true
+    },
     sleepStart: {
         type: String,
         required: true
