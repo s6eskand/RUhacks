2859f3689e25471b34be7fce55cd8f916ed53023
Sam <sam.eskandar00@hotmail.com>
2020-05-17 02:54:26 -0400

    add react-routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 import Home from './Views/Home';
+import eventCalendar from './Views/eventCalendar';
 import 'semantic-ui-css/semantic.min.css'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import 'react-calendar/dist/Calendar.css';
 
 function App() {
   return (
     <div>
-      <Home/>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/calendar" component={eventCalendar} />
+            </Switch>
+        </Router>
     </div>
   );
 }
